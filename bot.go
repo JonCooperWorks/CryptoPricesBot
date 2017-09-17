a66e45a9110871da897a3544a08df2c39f7ea16c
Jonathan Cooper <cooper@cooperthecoder.com>
2017-09-17 17:19:49 -0500

    Added shapeshift support and found bug where fiat -> crypto conversions only work for coincap supported coins

--- bot.go
+++ bot.go
@@ -4,11 +4,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"gopkg.in/telegram-bot-api.v4"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/hunterlong/shapeshift"
+	"gopkg.in/telegram-bot-api.v4"
+	"runtime"
 )
 
 /* Web Services config */
@@ -28,21 +31,17 @@ const (
 /* Controller routing table */
 var (
 	controllers = map[string]Controller{
-		START_COMMAND: Start,
-		QUOTE_COMMAND: Quote,
-		HELP_COMMAND:  Help,
+		START_COMMAND: StartCommand,
+		QUOTE_COMMAND: QuoteCommand,
+		HELP_COMMAND:  HelpCommand,
 	}
 )
 
-/* Currencies returned in coincap.io responses */
+/* Fiat currencies returned in coincap.io responses */
 var (
-	CURRENCIES = map[string]string{
+	FIAT_CURRENCIES = map[string]string{
 		"USD": "price_usd",
 		"EUR": "price_eur",
-		"LTC": "price_ltc",
-		"BTC": "price_btc",
-		"ETH": "price_eth",
-		"ZEC": "price_zec",
 	}
 
 	SYMBOLS = map[string]string{
@@ -51,19 +50,22 @@ var (
 		"LTC": " Ł",
 		"ETH": "Ξ",
 		"BTC": "฿",
-		"ZEC": "ZEC",
+		"ZEC": "ZEC ",
 	}
 )
 
 /* Messages */
 const (
-	WELCOME_MESSAGE string = "Ask me for prices with /quote (ticker). Example: /quote BTC or /quote BTC EUR"
-	HELP_MESSAGE    string = "Use me to get prices from https://coincap.io. Just type /quote (Ticker Symbol).\n" +
+	WELCOME_MESSAGE = "Ask me for prices with /quote (ticker). Example: /quote BTC or /quote BTC EUR."
+	HELP_MESSAGE    = "Use me to get prices from https://coincap.io and https://shapeshift.io.\n" +
+		"Just type /quote (First Symbol).\n" +
 		"For example, /quote BTC or /quote BTC EUR.\n" +
-		"Supported currencies: USD, EUR, BTC, LTC, ETH, ZEC"
-	COINCAP_BAD_RESPONSE_MESSAGE = "I can't read the response from https://coincap.io for '%s'"
-	COIN_NOT_FOUND_MESSAGE = "I can't find '%s' on https://coincap.io"
-	COINCAP_UNAVAILABLE_MESSAGE = "I'm having trouble reaching https://coincap.io. Try again later."
+		"Supported currencies: USD, EUR and all cryptocurrency pairs on https://shapeshift.io."
+	COINCAP_BAD_RESPONSE_MESSAGE         = "I can't read the response from https://coincap.io for '%s.'"
+	COINCAP_UNAVAILABLE_MESSAGE          = "I'm having trouble reaching https://coincap.io. Try again later."
+	COIN_NOT_FOUND_ON_COINCAP_MESSAGE    = "I can't find '%s' on https://coincap.io"
+	SHAPESHIFT_UNAVAILABLE_MESSAGE       = "I'm having trouble contacting https://shapeshift.io. Try again later."
+	COIN_NOT_FOUND_ON_SHAPESHIFT_MESSAGE = "I can't find %s/%s at either https://coincap.io or https://shapeshift.io."
 )
 
 type Controller func(*tgbotapi.BotAPI, tgbotapi.Update, []string)
@@ -71,22 +73,27 @@ type Command struct {
 	Controller Controller
 	Arguments  []string
 }
+type Quote struct {
+	Second string
+	First  string
+	Price  float64
+}
 
-func Start(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
+func StartCommand(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
 	reply(bot, update, WELCOME_MESSAGE)
 }
 
-func Help(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
+func HelpCommand(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
 	reply(bot, update, HELP_MESSAGE)
 }
 
-func Quote(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
+func QuoteCommand(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
 	if len(arguments) < 1 {
-		Help(bot, update, arguments)
+		HelpCommand(bot, update, arguments)
 		return
 	}
 
-	ticker := strings.ToUpper(arguments[0])
+	first := strings.ToUpper(arguments[0])
 	var comparisonCurrency string
 	if len(arguments) == 2 {
 		comparisonCurrency = strings.ToUpper(arguments[1])
@@ -94,55 +101,96 @@ func Quote(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
 		comparisonCurrency = "USD"
 	}
 
-	url := fmt.Sprintf(PRICE_API_ENDPOINT, ticker)
-	response, err := http.Get(url)
-	if err != nil || response.StatusCode != 200 {
-		reply(bot, update, COINCAP_UNAVAILABLE_MESSAGE)
-		return
-	}
-
-	if response.ContentLength == 2 {
-		reply(bot, update, fmt.Sprintf(COIN_NOT_FOUND_MESSAGE, ticker))
-		return
-	}
-	var coinQuoteResponse map[string]interface{}
-	err = json.NewDecoder(response.Body).Decode(&coinQuoteResponse)
+	quote, err := NewQuote(first, comparisonCurrency)
 	if err != nil {
-		log.Println(err.Error())
-		reply(bot, update, fmt.Sprintf(COINCAP_BAD_RESPONSE_MESSAGE, ticker))
+		reply(bot, update, err.Error())
 		return
 	}
-
-	quoteMessage, err := getQuoteFormat(comparisonCurrency, ticker, coinQuoteResponse)
-	if err != nil {
-		log.Println(err.Error())
-		Help(bot, update, []string{})
-	}
-	reply(bot, update, quoteMessage)
-
+	reply(bot, update, quote.String())
 }
 
-func getQuoteFormat(comparisonCurrency string, ticker string, coinQuoteResponse map[string]interface{}) (string, error) {
-	propertyName := CURRENCIES[comparisonCurrency]
-	if propertyName == "" {
-		return "", errors.New("Invalid currency passed: " + comparisonCurrency)
-	}
-	coinPrice := coinQuoteResponse[propertyName]
-	var quoteMessage string
-	switch coinPrice.(type) {
-	case float64, float32:
-		if coinPrice.(float64) < 1.00 {
-			quoteMessage = "1 %s = %s%.8f"
-		} else {
-			quoteMessage = "1 %s = %s%.2f"
-		}
-	}
+func isFiatInvolved(first, second string) bool {
+	return FIAT_CURRENCIES[first] != "" || FIAT_CURRENCIES[second] != ""
+}
 
-	symbol := SYMBOLS[comparisonCurrency]
-	if symbol == "" {
-		symbol = comparisonCurrency
+func coinCapCurrency(first, second string) string {
+	var currency = first
+	if FIAT_CURRENCIES[first] != "" {
+		currency = second
 	}
-	return fmt.Sprintf(quoteMessage, ticker, symbol, coinPrice), nil
+	return currency
+}
+
+func NewQuote(first, second string) (*Quote, error) {
+	log.Printf("Looking up %s/%s", first, second)
+	if isFiatInvolved(first, second) {
+		url := fmt.Sprintf(PRICE_API_ENDPOINT, first)
+		log.Printf("Looking up price at %s", url)
+		response, err := http.Get(url)
+		if err != nil {
+			return nil, errors.New(COINCAP_UNAVAILABLE_MESSAGE)
+		}
+
+		if response.ContentLength == 2 {
+			return nil, errors.New(fmt.Sprintf(COIN_NOT_FOUND_ON_COINCAP_MESSAGE, first))
+		}
+
+		var coinQuoteResponse map[string]interface{}
+		err = json.NewDecoder(response.Body).Decode(&coinQuoteResponse)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf(COINCAP_BAD_RESPONSE_MESSAGE, first))
+		}
+
+		var coinPrice float64
+		rawCoinPrice := coinQuoteResponse[FIAT_CURRENCIES[second]]
+		switch rawCoinPrice.(type) {
+		case float64, float32:
+			coinPrice = rawCoinPrice.(float64)
+		default:
+			log.Printf("Coin price for %s/%s is not a float or numeric type.", first, second)
+			return nil, errors.New(
+				fmt.Sprintf(COINCAP_BAD_RESPONSE_MESSAGE, first),
+			)
+		}
+		return &Quote{
+			First:  first,
+			Second: second,
+			Price:  coinPrice,
+		}, nil
+	} else {
+		pair := shapeshift.Pair{Name: fmt.Sprintf("%s_%s", first, second)}
+		log.Printf("Contacting https://shapeshift.io for %s", pair.Name)
+		info, err := pair.GetInfo()
+		if err != nil {
+			log.Println(err.Error())
+			return nil, errors.New(SHAPESHIFT_UNAVAILABLE_MESSAGE)
+		}
+
+		if info.ErrorMsg() != "" {
+			log.Println(info.ErrorMsg())
+			return nil, errors.New(fmt.Sprintf(COIN_NOT_FOUND_ON_SHAPESHIFT_MESSAGE, first, second))
+		}
+
+		return &Quote{
+			Second: second,
+			First:  first,
+			Price:  info.Rate,
+		}, nil
+	}
+}
+
+func (quote *Quote) String() string {
+	var quoteMessage string
+	if quote.Price < 1.00 {
+		quoteMessage = "1 %s = %s%.8f"
+	} else {
+		quoteMessage = "1 %s = %s%.2f"
+	}
+	symbol := SYMBOLS[quote.Second]
+	if symbol == "" {
+		symbol = quote.Second
+	}
+	return fmt.Sprintf(quoteMessage, quote.First, symbol, quote.Price)
 }
 
 func reply(bot *tgbotapi.BotAPI, update tgbotapi.Update, message string) {
@@ -152,16 +200,16 @@ func reply(bot *tgbotapi.BotAPI, update tgbotapi.Update, message string) {
 }
 
 func routeCommand(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	command, err := parseCommandFromUpdate(update)
+	command, err := NewCommand(update)
 	if err != nil {
 		log.Println(err.Error())
-		Help(bot, update, []string{})
+		HelpCommand(bot, update, []string{})
 		return
 	}
 	command.Controller(bot, update, command.Arguments)
 }
 
-func parseCommandFromUpdate(update tgbotapi.Update) (*Command, error) {
+func NewCommand(update tgbotapi.Update) (*Command, error) {
 	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 	parts := parseArgumentsFromUpdate(update.Message.Text)
 	if len(parts) < 1 {
@@ -170,7 +218,7 @@ func parseCommandFromUpdate(update tgbotapi.Update) (*Command, error) {
 
 	if !update.Message.IsCommand() {
 		return &Command{
-			Controller: Quote,
+			Controller: QuoteCommand,
 			Arguments:  parts,
 		}, nil
 	}
@@ -190,14 +238,25 @@ func parseCommandFromUpdate(update tgbotapi.Update) (*Command, error) {
 		Arguments:  parts[1:],
 	}, nil
 }
+
 func parseArgumentsFromUpdate(message string) []string {
 	return strings.Split(message, " ")
+}
+
+func worker(updates <-chan tgbotapi.Update, bot *tgbotapi.BotAPI) {
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		routeCommand(bot, update)
+	}
 }
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	bot.Debug = os.Getenv("DEBUG") != ""
@@ -208,11 +267,15 @@ func main() {
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-
-		go routeCommand(bot, update)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	// Goroutine pool for processing messages.
+	for i := 0; i < runtime.NumCPU() - 1; i++ {
+		go worker(updates, bot)
+	}
+	// Block on the main thread so we don't exit
+	worker(updates, bot)
 }
+
