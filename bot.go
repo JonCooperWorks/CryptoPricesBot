5f0333158f15aa5e8eb627cdb40c263ec3e5a080
Jonathan Cooper <cooper@cooperthecoder.com>
2017-09-17 00:51:45 -0500

    Added support for currency conversions returned by coincap.io

--- bot.go
+++ bot.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"gopkg.in/telegram-bot-api.v4"
 	"log"
@@ -10,6 +11,7 @@ import (
 	"strings"
 )
 
+/* Web Services config */
 const (
 	PRICE_API_ENDPOINT = "https://coincap.io/page/%s"
 	USERNAME_SEPARATOR = "@"
@@ -23,6 +25,7 @@ const (
 	HELP_COMMAND  = "/help"
 )
 
+/* Controller routing table */
 var (
 	controllers = map[string]Controller{
 		START_COMMAND: Start,
@@ -31,14 +34,39 @@ var (
 	}
 )
 
+/* Currencies returned in coincap.io responses */
+var (
+	CURRENCIES = map[string]string{
+		"USD": "price_usd",
+		"EUR": "price_eur",
+		"LTC": "price_ltc",
+		"BTC": "price_btc",
+		"ETH": "price_eth",
+		"ZEC": "price_zec",
+	}
+
+	SYMBOLS = map[string]string{
+		"USD": "$",
+		"EUR": "€",
+		"LTC": " Ł",
+		"ETH": "Ξ",
+		"BTC": "₿",
+		"ZEC": "ZEC",
+	}
+)
+
 /* Messages */
 var (
 	WELCOME_MESSAGE string = "Ask me for prices with /quote (ticker). Example: /quote BTC"
 	HELP_MESSAGE    string = "Use me to get prices from https://coincap.io. Just type /quote (Ticker Symbol). For " +
-		"example, /quote BTC."
+		"example, /quote BTC or /quote BTC EUR"
 )
 
 type Controller func(*tgbotapi.BotAPI, tgbotapi.Update, []string)
+type Command struct {
+	Controller Controller
+	Arguments  []string
+}
 
 func Start(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
 	reply(bot, update, WELCOME_MESSAGE)
@@ -55,6 +83,13 @@ func Quote(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
 	}
 
 	ticker := strings.ToUpper(arguments[0])
+	var comparisonCurrency string
+	if len(arguments) == 2 {
+		comparisonCurrency = strings.ToUpper(arguments[1])
+	} else {
+		comparisonCurrency = "USD"
+	}
+
 	url := fmt.Sprintf(PRICE_API_ENDPOINT, ticker)
 	response, err := http.Get(url)
 	if err != nil || response.StatusCode != 200 {
@@ -73,26 +108,31 @@ func Quote(bot *tgbotapi.BotAPI, update tgbotapi.Update, arguments []string) {
 		reply(bot, update, fmt.Sprintf("Error decoding response for %s", ticker))
 		return
 	}
-	coinPriceUsd := coinQuoteResponse["price_usd"]
-	quoteMessage := getQuoteFormat(coinPriceUsd)
-	reply(bot, update,  fmt.Sprintf(quoteMessage, ticker, coinPriceUsd))
+
+	quoteMessage := getQuoteFormat(comparisonCurrency, ticker, coinQuoteResponse)
+	reply(bot, update, quoteMessage)
 
 }
 
-func getQuoteFormat(coinPriceUsd interface{}) string {
+func getQuoteFormat(comparisonCurrency string, ticker string, coinQuoteResponse map[string]interface{}) string {
+	propertyName := CURRENCIES[comparisonCurrency]
+	if propertyName == "" {
+		propertyName = "price_usd"
+	}
+	coinPrice := coinQuoteResponse[propertyName]
 	var quoteMessage string
-	switch coinPriceUsd.(type) {
+	switch coinPrice.(type) {
 	case float64, float32:
-		if coinPriceUsd.(float64) < 0.99 {
-			quoteMessage = "1 %s = USD$%.8f"
+		if coinPrice.(float64) < 0.99 {
+			quoteMessage = "1 %s = %s%s%.8f"
 		} else {
-			quoteMessage = "1 %s = USD$%.2f"
+			quoteMessage = "1 %s = %s%.2f"
 		}
 	}
 
-	return quoteMessage
+	symbol := SYMBOLS[comparisonCurrency]
+	return fmt.Sprintf(quoteMessage, ticker, symbol, coinPrice)
 }
-
 
 func reply(bot *tgbotapi.BotAPI, update tgbotapi.Update, message string) {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, message)
@@ -101,30 +141,46 @@ func reply(bot *tgbotapi.BotAPI, update tgbotapi.Update, message string) {
 }
 
 func routeCommand(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	if !update.Message.IsCommand() {
-		command := update.Message.Text
-		Quote(bot, update, []string{command})
-	} else {
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		parts := strings.Split(update.Message.Text, " ")
-		if len(parts) < 1 {
-			Help(bot, update, []string{})
-			return
-		}
-
-		controllerName := parts[0]
-		if strings.Contains(controllerName, BOT_NAME) {
-			controllerName = strings.Split(controllerName, USERNAME_SEPARATOR)[0]
-		}
-
-		controller := controllers[controllerName]
-		if controller == nil {
-			Help(bot, update, []string{})
-			return
-		}
-
-		controller(bot, update, parts[1:])
+	command, err := parseCommandFromUpdate(update)
+	if err != nil {
+		Help(bot, update, []string{})
+		return
 	}
+	command.Controller(bot, update, command.Arguments)
+}
+
+func parseCommandFromUpdate(update tgbotapi.Update) (*Command, error) {
+	if !update.Message.IsCommand() {
+		arguments := parseArgumentsFromUpdate(update.Message.Text)
+		return &Command{
+			Controller: Quote,
+			Arguments:  arguments,
+		}, nil
+	}
+
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	parts := parseArgumentsFromUpdate(update.Message.Text)
+	if len(parts) < 1 {
+		return nil, errors.New(HELP_MESSAGE)
+	}
+
+	controllerName := parts[0]
+	if strings.Contains(controllerName, BOT_NAME) {
+		controllerName = strings.Split(controllerName, USERNAME_SEPARATOR)[0]
+	}
+
+	controller := controllers[controllerName]
+	if controller == nil {
+		return nil, errors.New(HELP_MESSAGE)
+	}
+
+	return &Command{
+		Controller: controller,
+		Arguments:  parts[1:],
+	}, nil
+}
+func parseArgumentsFromUpdate(message string) []string {
+	return strings.Split(message, " ")
 }
 
 func main() {
